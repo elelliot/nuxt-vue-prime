e7552d170917cd69c238ba7843d260c53edb7b23
ElElliot <elliot_150894@hotmail.com>
2023-04-12 00:47:10 -0700

    Added menubar in about

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 
-import { ref } from 'vue';
 import SelectButton from 'primevue/selectbutton';
-
+import Menubar from 'primevue/menubar';
 
 const options = ref([
     { name: 'En venta', value: 1 },
@@ -22,22 +21,135 @@ watchEffect(() => {
     console.log(value.value)
     console.log(value2.value)
 })
+
+const items = ref([
+    {
+        label: 'Categoria',
+        items: [
+            {
+                label: 'Left',
+                icon: 'pi pi-fw pi-align-left'
+            },
+            {
+                label: 'Right',
+                icon: 'pi pi-fw pi-align-right'
+            },
+            {
+                label: 'Center',
+                icon: 'pi pi-fw pi-align-center'
+            },
+            {
+                label: 'Justify',
+                icon: 'pi pi-fw pi-align-justify'
+            }
+        ]
+    },
+    {
+        label: 'Conjunto',
+        items: [
+            {
+                label: 'Left',
+                icon: 'pi pi-fw pi-align-left'
+            },
+            {
+                label: 'Right',
+                icon: 'pi pi-fw pi-align-right'
+            },
+            {
+                label: 'Center',
+                icon: 'pi pi-fw pi-align-center'
+            },
+            {
+                label: 'Justify',
+                icon: 'pi pi-fw pi-align-justify'
+            }
+        ]
+    },
+    {
+        label: 'Recamaras',
+        icon: 'pi pi-home',
+        items: [
+            {
+                label: 'Left',
+                icon: 'pi pi-fw pi-align-left'
+            },
+            {
+                label: 'Right',
+                icon: 'pi pi-fw pi-align-right'
+            },
+            {
+                label: 'Center',
+                icon: 'pi pi-fw pi-align-center'
+            },
+            {
+                label: 'Justify',
+                icon: 'pi pi-fw pi-align-justify'
+            }
+        ]
+    },
+    {
+        label: 'Baños',
+        icon: 'pi pi-map-marker',
+        items: [
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {
+                        label: 'Save',
+                        icon: 'pi pi-fw pi-calendar-plus'
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            },
+            {
+                label: 'Archieve',
+                icon: 'pi pi-fw pi-calendar-times',
+                items: [
+                    {
+                        label: 'Remove',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Precio',
+        icon: 'pi pi-dollar'
+    },
+    {
+        label: 'Precio',
+        icon: 'pi pi-sliders-h'
+    }
+]);
 </script>
 
 <template>
-    <h3>Categoria</h3>
-    <div class="p-fluid">
-        <SelectButton v-model="value" :options="options" optionLabel="name" class="w-6" />
+    <div class="flex flex-column align-items-center">
+        <div class="w-6">
+            <h3>Categoria</h3>
+        </div>
+        <div class="p-fluid w-6">
+            <SelectButton v-model="value" :options="options" optionLabel="name" class="" />
+        </div>
     </div>
-    <h3>Que tipo de propiedad estas buscando?</h3>
-    <div class="p-">
-        <SelectButton v-model="value2" :options="options2" optionLabel="name" />
+    
+    <div class="flex flex-column align-items-center">
+        <div class="w-6">
+            <h3>Que tipo de propiedad estas buscando?</h3>
+        </div>
+        <div class="p-fluid w-6">
+            <SelectButton v-model="value2" :options="options2" optionLabel="name" class="white-space-nowrap" />
+        </div>
+    </div>
+
+
+    <!-- MenuBar Attempt -->
+    <div class="">
+        <Menubar :model="items" />
     </div>
 </template>
-
-<style scoped >
-/* :deep(.p-button):nth-child(1n) {
-    width: 50%;
-} */
-</style>
-
