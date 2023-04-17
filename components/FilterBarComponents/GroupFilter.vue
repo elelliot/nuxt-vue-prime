b895866c9c5a6d57fe045b6c6861c744a023f75f
Elliot Miranda <elliot_150894@hotmail.com>
2023-04-17 11:04:57 -0700

    Fixes Implementation

--- components/FilterBarComponents/GroupFilter.vue
+++ components/FilterBarComponents/GroupFilter.vue
@@ -2,6 +2,7 @@
 import Button from 'primevue/button';
 import OverlayPanel from 'primevue/overlaypanel';
 import ToggleButton from 'primevue/togglebutton';
+import { faker } from '@faker-js/faker';
 
 const downArrow = ref(true)
 
@@ -20,6 +21,13 @@ const toggle = (event: any) => {
     downArrow.value = !downArrow.value
 }
 
+const dummyNum: Number[] = []
+
+onMounted(() => {
+    for(let i = 0 ; i < 7 ; i++)
+        dummyNum.push(faker.datatype.number({ min: 0, max: 10000 }))
+});
+
 </script>
 <template>
     <Button @click="toggle" class="" iconPos="right" :icon="downArrow ? 'pi pi-angle-down' : 'pi pi-angle-up'"
@@ -28,13 +36,13 @@ const toggle = (event: any) => {
         <div class="flex flex-col gap-4 w-full">
 
             <div class="grid grid-cols-2 gap-4">
-                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="'Desarrollo (6456)'" :offLabel="'Desarrollo (6456)'" />
-                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn2" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="'Fraccionamientos (9875)'" :offLabel="'Fraccionamientos (9875)'" />
-                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn3" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="'Privada (1632)'" :offLabel="'Privada (1632)'" />
-                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn4" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="'Residencial (1517)'" :offLabel="'Residencial (1517)'" />
-                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn5" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="'Condominios (2135)'" :offLabel="'Condominios (2135)'" />
-                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn6" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="'Country Club (1574)'" :offLabel="'Country Club (1574)'" />
-                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn7" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="'Club de Golf (121)'" :offLabel="'Club de Golf (121)'" />
+                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="`Desarrollo (${dummyNum[0]})`" :offLabel="`Desarrollo (${dummyNum[0]})`" />
+                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn2" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="`Fraccionamientos (${dummyNum[1]})`" :offLabel="`Fraccionamientos (${dummyNum[1]})`" />
+                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn3" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="`Privada (${dummyNum[2]})`" :offLabel="`Privada (${dummyNum[2]})`" />
+                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn4" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="`Residencial (${dummyNum[3]})`" :offLabel="`Residencial (${dummyNum[3]})`" />
+                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn5" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="`Condominios (${dummyNum[4]})`" :offLabel="`Condominios (${dummyNum[4]})`" />
+                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn6" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="`Country Club (${dummyNum[5]})`" :offLabel="`Country Club (${dummyNum[5]})`" />
+                <ToggleButton class="whitespace-nowrap" iconPos="right" v-model="isOn7" offIcon="pi pi-filter" onIcon="pi pi-filter-fill" :onLabel="`Club de Golf (${dummyNum[6]})`" :offLabel="`Club de Golf (${dummyNum[6]})`" />
             </div>
 
 
