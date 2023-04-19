e87a2408c854b5cc3b309a494204c840481c81b7
ElElliot <elliot_150894@hotmail.com>
2023-04-19 01:28:32 -0700

    Adds ContactFormComponents directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    components: {
-        "dirs": [
-            {
-              "path": "~/components/FilterBarComponents",
-              
-            },
-            "~/components"
-          ]
-    },
-    css: [
-        "@/assets/css/main.css",
-        // "primevue/resources/themes/lara-light-blue/theme.css",
-        // "primevue/resources/themes/mdc-dark-deeppurple/theme.css",
-        "primevue/resources/themes/viva-light/theme.css",
-        "primevue/resources/primevue.css",
-        "primeicons/primeicons.css",
-    ],
-    modules: [
-        '@unocss/nuxt',
-      ],
-	build: {
-		transpile: ["primevue"]
-	}
+  components: {
+    "dirs": [
+      { "path": "~/components/ContactFormComponents" },
+      { "path": "~/components/FilterBarComponents" },
+      "~/components"
+    ]
+  },
+  css: [
+    "@/assets/css/main.css",
+    // "primevue/resources/themes/lara-light-blue/theme.css",
+    // "primevue/resources/themes/mdc-dark-deeppurple/theme.css",
+    "primevue/resources/themes/viva-light/theme.css",
+    "primevue/resources/primevue.css",
+    "primeicons/primeicons.css",
+  ],
+  modules: [
+    '@unocss/nuxt',
+  ],
+  build: {
+    transpile: ["primevue"]
+  }
 })
