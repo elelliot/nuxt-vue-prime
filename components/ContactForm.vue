79bdfa3587f36520b3ee9ddc99ed53e29c38d5d6
Elliot Miranda <elliot_150894@hotmail.com>
2023-04-18 17:17:57 -0700

    Adds Contact Form component

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -3,6 +3,14 @@ import InputText from 'primevue/inputtext';
 import InputMask from 'primevue/inputmask';
 import Textarea from 'primevue/textarea';
 import Button from 'primevue/button';
+import SelectButton from 'primevue/selectbutton';
+
+
+const contactOptions = ref([
+    { name: 'Mensaje', value: 1 },
+    { name: 'Agendar Visita', value: 2 }
+]);
+
 
 const name = ref('')
 const tel = ref('')
@@ -12,7 +20,9 @@ const message = ref(messagePlaceHolder)
 
 </script>
 <template>
+    <SelectButton :options="contactOptions" :unselectable="false" />
     <div class="flex flex-col gap-4">
+        <span class="text-lg">Enviar Mensaje</span>
         <div class="flex gap-4">
 
             <div class="flex flex-col">
@@ -36,8 +46,16 @@ const message = ref(messagePlaceHolder)
             <Textarea v-model="message" id="message" maxlength="500" autoResize rows="3" cols="30" />
         </div>
 
-        <Button type="submit" label="Enviar mensaje" rounded class="bg-blue-400 border-blue-400" />
-        <Button type="submit" label="O enviar un Whatsapp" icon="pi pi-whatsapp" iconPos="right" rounded
-            class="hover:(bg-green-800 border-green-800) bg-green-500 border-green-500" />
+        <Button type="submit" label="Enviar mensaje" class="bg-blue-400 border-blue-400" />
+        <Button class="hover:(bg-green-800 border-green-800) bg-green-500 border-green-500">
+            <div class="w-full flex justify-center gap-4 items-center">
+                <span class="">O enviar un WhatsApp</span> 
+                <i class="pi pi-whatsapp"></i>
+            </div>
+        </Button>
+
+        <span class="text-xs text-slate-500	">Al hacer clic en “Enviar” aceptas nuestros <NuxtLink to="/about"
+                class="text-slate-500">Términos y Condiciones, Políticas de Privacidad</NuxtLink> y recibir comunicaciones
+            de marketing</span>
     </div>
 </template>
