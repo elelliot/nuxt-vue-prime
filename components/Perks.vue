573a0bad1461d8a4089d7a64bd68b96f7ca6a7e8
Elliot Miranda <elliot_150894@hotmail.com>
2023-04-21 11:22:30 -0700

    Fixes styles

--- components/Perks.vue
+++ components/Perks.vue
@@ -4,9 +4,9 @@ const props = defineProps<{
 }>()
 </script>
 <template>
-    <div class="flex flex-col items-center gap-1">
+    <div class="flex flex-col gap-1">
         <div class="w-14 h-14 flex justify-center items-center border-solid border-2 rounded border-bluegray">
-        <i :class="`text-[48px] ${props.perkIcon}`"></i>
+        <i :class="`text-[40px] ${props.perkIcon}`"></i>
         </div>
         <span class="text-blueGray text-xs font-600"><slot name="text"></slot></span>
     </div>
