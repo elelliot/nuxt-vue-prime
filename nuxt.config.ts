037f94d33c7c02e7e6020da4e8f5b50e08a34fab
Elliot Miranda <elliot_150894@hotmail.com>
2023-05-08 10:20:09 -0700

    Adds Code Cleaning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  
+
   components: {
     "dirs": [
       { "path": "~/components/ContactFormComponents" },
@@ -10,8 +10,6 @@ export default defineNuxtConfig({
   },
   css: [
     "@/assets/css/main.css",
-    // "primevue/resources/themes/lara-light-blue/theme.css",
-    // "primevue/resources/themes/mdc-dark-deeppurple/theme.css",
     "primevue/resources/themes/viva-light/theme.css",
     "primevue/resources/primevue.css",
     "primeicons/primeicons.css",
